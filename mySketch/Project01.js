493b252cef401999d04e9ea75a44e552120cc33f
jgomez002 <jdg8858@nyu.edu>
2021-11-09 19:58:49 -0500

    Push
    Adjusted code, it works but I need to strengthen the visuals to better convey my adjective. Will be implementing feedback suggestions in next few pushes

--- mySketch/Project01.js
+++ mySketch/Project01.js
@@ -1,3 +1,7 @@
+var sections = 0;
+var x,y;
+
+
 function setup() {
 createCanvas (windowWidth, windowHeight);
 background (0);
@@ -8,6 +12,9 @@ function draw() {
 //print(mouseX);
   
 if (millis()<8000){
+  circlespawn();
+}
+  if (millis()<9000){
   circlespawn();
 }
   
@@ -26,14 +33,29 @@ if (millis()>13000 && millis ()<15000){
   dogbox();
 }
 */
+if (millis()>12000 && millis()<14000){  
+  background(0);
+  animal();
+}
+  
 if (millis()>15000 ){
-   doginbox();
-fill(66, 140, 73);
-for(var b=0;b<=width;b=b+250){
-ellipse(b,700,500,500);
+ background(0); 
+ //dogbunnyidk();
+  scene1();
+  dogbunnyidk();
+  
+}  
+}
+   
+  //else if (mouseX < 66) {
+    //rect(33, 0, 33, 100); // Middle
+  //}
+  //else {
+    //rect(66, 0, 33, 100); // Right
+  //}
 
-}
-}
+
+//}
 
 
 
@@ -45,7 +67,7 @@ function circlespawn(){
   fill (255, random(255));
   ellipse ((random(1728)), (random(1117)),50,50);
   
-  }
+  
 }
 
   /*function clearout(){
@@ -57,11 +79,22 @@ function circlespawn(){
 */
 
 function doggocircle(){
+  fill(255);
   background(0);
   ellipse (199,335,50,50);
 
 }
-/*
+
+function animal(){
+  doggocircle();
+  fill(255);
+  ellipse(205,315,25,25);
+  ellipse(190,315,25,25);
+  fill (0);
+  ellipse(205,325,10,10);
+  ellipse(190,325,10,10);
+}
+
 function dogbox(){
   //print(mouseX,mouseY);
   noStroke();
@@ -100,16 +133,16 @@ function dogbox(){
   rect (161, 331, 100,50);
   
 }
-*/
-function doginbox (){
-background(0);
+
+function dogbunnyidk (){
 
   push();
   translate(mouseX-250,mouseY-300);
   fill(255);
   //dogbox();
-  doggocircle();
   noStroke();
+  ellipse (199,335,50,50);
+  
 
   fill(255);
   ellipse(205,315,25,25);
@@ -118,15 +151,43 @@ background(0);
   ellipse(205,325,10,10);
   ellipse(190,325,10,10);
   pop();
+}
+function scene1(){
+  
 fill(255);  
 for (var s=0; s<400; s = s+1){ 
   rect(s*windowWidth/3, 0, 5, height);
-}  
+  for(var b=0;b<=width;b=b+250){
+fill(66, 140, 73);
+ellipse(b,700,500,500);
+fill (163, 123, 119);
+rect(b*2,height*3/8,200,250);
+fill(100);
+rect(50+b*2,height*0.55,50,90);
+triangle(b*2,width/4.8,190+b*2,width/4.8,85+b*2,width/7);
+
 }
+} 
+  if (mouseX < windowWidth*1/3) {
+    fill(0);
+    rect(0, 0, windowWidth*1/3, windowHeight); 
+}
+  else if( mouseX <windowWidth*2/3){
+    fill(0);
+    rect(windowWidth*1/3, 0, windowWidth*1/3, windowHeight);       
+          }
+  else {
+    fill(0);
+    rect(windowWidth*2/3, 0, windowWidth*1/3, windowHeight);    
+  } 
+  
+}
+
+
+
 
 //function scene1(){
  // fill(66, 140, 73);
  // ellipse(windowWidth/(1/3),windowHeight/(2/3),500,500);
 //}
-
 
